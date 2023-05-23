863d7c9dca1f36068c15f37172eea1c133971323
teaguejobs <teaguejobs@gmail.com>
2023-05-23 06:01:45 +0100

    first commit

--- main.tf
+++ main.tf
@@ -1,12 +1,31 @@
+# ------------- root/main.tf----------------------
+# Get list of availability zones
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
 provider "aws" {
-  region = "us-east-1"
+  region = var.region
 }
 
 # Create VPC
 resource "aws_vpc" "main" {
-  cidr_block                     = "172.16.0.0/16"
-  enable_dns_support             = "true"
-  enable_dns_hostnames           = "true"
-  enable_classiclink             = "false"
-  enable_classiclink_dns_support = "false"
+  cidr_block                     = var.vpc_cidr
+  enable_dns_support             = var.enable_dns_support
+  enable_dns_hostnames           = var.enable_dns_hostnames
+  enable_classiclink             = var.enable_classiclink
+  enable_classiclink_dns_support = var.enable_dns_support
+
 }
+
+
+# Create public subnets
+resource "aws_subnet" "public" {
+  count                   = var.preferred_number_of_public_subnets == null ? length(data.aws_availability_zones.available.names) : var.preferred_number_of_public_subnets
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = cidrsubnet(var.vpc_cidr, 4, count.index)
+  map_public_ip_on_launch = true
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+
+}
+
